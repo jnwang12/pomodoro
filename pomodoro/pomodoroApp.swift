d2ae7e07830afd8838ca2efcef79df434ad9046f
jnwang12 <joywang354@gmail.com>
2026-07-30 01:15:41 -0700

    Initial Commit

---
+++ pomodoro/pomodoroApp.swift
@@ -0,0 +1,17 @@
+//
+//  pomodoroApp.swift
+//  pomodoro
+//
+//  Created by Joy Wang on 7/30/26.
+//
+
+import SwiftUI
+
+@main
+struct pomodoroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
